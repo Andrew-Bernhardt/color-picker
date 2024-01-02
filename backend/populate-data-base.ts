f62cff9062abb63545f4420bfea7d9fc32100130
Andrew Bernhardt <aeb30@email.sc.edu>
2024-01-02 15:46:14 -0500

    populate-data-base.ts is now working and populates data field

--- backend/populate-data-base.ts
+++ backend/populate-data-base.ts
@@ -1,19 +1,23 @@
-import { IColorBlock } from "../frontend/src/DashBoard";
-import { APIURL } from "../frontend/src/GlobalVariables";
+const APIURL="http://localhost:3001/colorpicker"
+interface IColorBlock {
+    color: string
+    votes: number
+}
 
 function getArrayOfColors (numBlocks: number) {
     let lastColor = "";
     const retColorBlocks: IColorBlock[] = [];
     for(let i=0;i<numBlocks;i++) {
-        const colorObj =  { _id: i, color: '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6), votes : 0, __v: ''}
+        const colorObj =  {color: '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6), votes : 0 }
         retColorBlocks.push(colorObj);
         lastColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
     }
     return retColorBlocks;
 }
 
-function getSingleColor () {
-    const colorObj =  { color: '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6), votes : 0, __v: ''}
+function getSingleColor() {
+    const colorObj =  { color: '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6), votes : 0 }
+    console.log("HERE IS THE COLOR" + colorObj)
     return colorObj;
 }
 
@@ -21,21 +25,19 @@ function postSingleColor() {
     fetch(APIURL + '/', {
         method: "POST",
         headers: {'Content-Type': 'application/json'}, 
-        body: JSON.stringify(getSingleColor)
+        body: JSON.stringify(getSingleColor())
     }).then(res => {
-        console.log("Request complete! response:", res);
+        console.log(res);
     });
-    
 }
 
 function postMultipleColors(numColors: number=100): void {
 
-    for(let i=0;i< numColors; i++) {
-        postSingleColor;
+    for(let i=0;i<numColors;i++) {
+        postSingleColor();
     }
 }
-
-postMultipleColors(100)
+postMultipleColors(1000)
 
 
 
