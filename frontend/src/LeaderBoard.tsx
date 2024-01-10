de9e21312c3fa7b7b27d3e6fc39fccd42e4fbc1e
Andrew Bernhardt <aeb30@email.sc.edu>
2024-01-10 14:21:09 -0500

    Created 1st place leaderboard effect
    it is responsive to whoever is in first place

--- frontend/src/LeaderBoard.tsx
+++ frontend/src/LeaderBoard.tsx
@@ -8,15 +8,18 @@ export default function LeaderBoard( {colorBlocks, children} ) {
   colorBlocksCopy = colorBlocksCopy.slice(0,15);
   const topColor = '#'+colorBlocksCopy[0].color
 
-  const flames = Array.from(Array(10).keys())
+  const flames = Array.from(Array(100).keys())
   
   return (
     <div className="leaderboard-flex">
       <h2>{children} LeaderBoard</h2>
       <div className="flame-animation">
-        {flames.map((flame, i) => <div className="flame" style={{backgroundColor: topColor}}></div>)}
+        {flames.map((flame, i) => 
+          <div className="flame" style={{backgroundColor: topColor}}></div>)
+        }
       </div>
       <ul> 
+        <li className="leaderboard-item top-block" style={{backgroundColor: topColor, color: topColor}}> yo</li>
         {
           colorBlocksCopy.map((cb)=> 
             <li key={cb.color} className="leaderboard-item" style={{backgroundColor: "#"+cb.color}}>
